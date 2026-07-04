a955e28a3178a758022ea43893a02891d8b3d85d
vishalcse001 <vishalyadav.95055@gmail.com>
2026-07-04 12:19:13 +0530

    Fix case-sensitive import path for User model

--- server/models/user.js
+++ server/models/user.js
@@ -6,4 +6,4 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('user', userSchema);
